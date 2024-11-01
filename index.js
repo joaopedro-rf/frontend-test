dca0cabbb24394f44795a7bc25a6046790820e2b
Joao Pedro <joaopedro-rf@Hotmail.com>
2024-11-01 15:31:59 -0300

    update index file

--- index.js
+++ index.js
@@ -5,19 +5,9 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; " +
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live; " +
-      "style-src 'self' 'unsafe-inline'; " +
-      "img-src 'self' data: https:; " +
-      "connect-src 'self' https://vercel.live; " +
-      "font-src 'self'; " +
-      "frame-src 'self' https://vercel.live/; " +
-      "form-action 'self';"
-  );
-  next();
+app.get('/', (req, res) => {
+
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
