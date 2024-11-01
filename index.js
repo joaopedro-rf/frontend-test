b30a599192c4e0a42f451adde5a1f877fbd7fa78
Joao Pedro <joaopedro-rf@Hotmail.com>
2024-11-01 14:49:45 -0300

    .env

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT);
+}
