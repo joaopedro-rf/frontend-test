43bfabb74986bf9526036a616e34de544343a5d6
Joao Pedro <joaopedro-rf@Hotmail.com>
2024-11-01 15:06:09 -0300

    vercel headers

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const path = require('path');
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live; " +
+    "style-src 'self' 'unsafe-inline'; " +
+    "img-src 'self' data: https:; " +
+    "connect-src 'self' https://vercel.live; " +
+    "font-src 'self'; " +
+    "frame-src 'self';"
+  );
+  next();
 });
 
 
