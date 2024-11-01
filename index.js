f36cdd8ad6ddbc3531822022c0731d308b73ba25
Joao Pedro <joaopedro-rf@Hotmail.com>
2024-11-01 15:03:19 -0300

    add vercel configs

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use(express.static(path.join(__dirname, "public")));
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
