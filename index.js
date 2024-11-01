6a9093df18f619481eb71ade66e788d1cbb5508a
Joao Pedro <joaopedro-rf@Hotmail.com>
2024-11-01 15:25:11 -0300

    Update configuration for Render deployment

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   res.setHeader(
-    'Content-Security-Policy',
+    "Content-Security-Policy",
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live; " +
-    "style-src 'self' 'unsafe-inline'; " +
-    "img-src 'self' data: https:; " +
-    "connect-src 'self' https://vercel.live; " +
-    "font-src 'self'; " +
-    "frame-src 'self';"
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live; " +
+      "style-src 'self' 'unsafe-inline'; " +
+      "img-src 'self' data: https:; " +
+      "connect-src 'self' https://vercel.live; " +
+      "font-src 'self'; " +
+      "frame-src 'self' https://vercel.live/; " +
+      "form-action 'self';"
   );
   next();
 });
 
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT);
-}
-
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
